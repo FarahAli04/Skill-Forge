53ba265ae1ba55b7c179ce0e1e18b3e4bb58666b
Habiba-Aboghazala9 <es.habiba.salah2023@alexu.edu.eg>
2025-11-16 17:09:13 +0200

    Create Manager.java

---
+++ SkillForge/src/main/java/com/mycompany/skillforge/Manager.java
@@ -0,0 +1,59 @@
+package com.mycompany.skillforge;
+
+import java.util.ArrayList;
+import java.util.UUID;
+        
+public class Manager {
+    private static ArrayList<User> users = new ArrayList<>();
+    private static User loggedInUser = null;
+
+    
+    public static String signup(String username, String email, String password, String role) {
+
+        if (!InputValidator.isNotEmpty(username) ||
+            !InputValidator.isNotEmpty(email) ||
+            !InputValidator.isValidEmail(email) ||
+            !InputValidator.isValidPassword(password)) {
+            return "Invalid Input!";
+        }
+
+       
+        for (User u : users) {
+            if (u.getEmail().equalsIgnoreCase(email))
+                return "Email already exists!";
+        }
+
+        String userId = UUID.randomUUID().toString();
+        String hashed = Password.hashPassword(password);
+
+        User newUser;
+
+        if (role.equalsIgnoreCase("student")) {
+            newUser = new Student(userId, username, email, hashed);
+        } else {
+            newUser = new Instructor(userId, username, email, hashed);
+        }
+
+        users.add(newUser);
+
+        return "Signup successful!";
+    }
+
+    
+    public static String login(String email,String password,String role) {
+        String hashed = Password.hashPassword(password);
+
+        for (User u : users) {
+            if (u.getEmail().equalsIgnoreCase(email) && u.getPasswordHash().equals(hashed)&&u.getRole().equalsIgnoreCase(role)) {
+                loggedInUser = u;
+                return "Login successful ";
+            }
+        }
+
+        return "Invalid email or password or role";
+    }
+
+    public static void logout() {
+        loggedInUser = null;
+    }
+}
